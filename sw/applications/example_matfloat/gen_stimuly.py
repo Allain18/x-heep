38ddde8e70141e7d88f8c2e5ecf37d2bfd5c7d91
davide schiavone <davide@openhwgroup.org>
2026-02-13 17:12:47 +0100

    fix header

--- sw/applications/example_matfloat/gen_stimuly.py
+++ sw/applications/example_matfloat/gen_stimuly.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-## Copyright 2024 EPFL
+## Copyright 2026 EPFL
 ## Solderpad Hardware License, Version 2.1, see LICENSE.md for details.
 ## SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
 
