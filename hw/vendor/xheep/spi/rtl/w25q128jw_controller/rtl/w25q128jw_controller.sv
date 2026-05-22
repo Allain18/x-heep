e6cf40c13c6bba925ca1b66397e14b8811c56b40
Alain Girard <alain.girard@epfl.ch>
2026-05-22 14:45:11 +0200

    bug

--- hw/vendor/xheep/spi/rtl/w25q128jw_controller/rtl/w25q128jw_controller.sv
+++ hw/vendor/xheep/spi/rtl/w25q128jw_controller/rtl/w25q128jw_controller.sv
@@ -484,7 +484,7 @@ module w25q128jw_controller
             // See hw/vendor/lowrisc_opentitan_spi_host/data/spi_host.hjson for status register bit mapping
             // See hw/vendor/lowrisc_opentitan_spi_host/rtl/spi_host_reg_pkg.sv for TXQD depth definition
             if (external_spi_host_hw2reg_status_i.txqd.d < SPI_FLASH_TX_FIFO_DEPTH[7:0]) begin
-              if ((memio_mode_q && QUAD_AVAILABLE) || reg2hw.control.rnw.q) begin
+              if ((memio_mode_q && QUAD_AVAILABLE) || reg2hw.control.quad.q) begin
                 read_state_d = READ_SPI_SEND_CMD_1_QUAD;
               end else begin
                 read_state_d = READ_SPI_FILL_TX_FIFO;
