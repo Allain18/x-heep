7a2292329c2c2b11f84ab69da42a0343abcf4545
Patrick Pataky <patrick.pataky@epfl.ch>
2026-05-30 20:47:41 +0200

    Replaced paramter types into their full type

--- hw/vendor/xheep/spi/rtl/w25q128jw_controller/rtl/cache/cache_reg_pkg.sv
+++ hw/vendor/xheep/spi/rtl/w25q128jw_controller/rtl/cache/cache_reg_pkg.sv
@@ -47,8 +47,8 @@ package cache_reg_pkg;
     cache_op_e                            op;         // read or write
     addr_t                                addr;       // address to read/write, byte-addressable
     logic [SECTOR_SIZE_WORDS_WIDTH-1:0]   word_count; // number of words to read/write in current request
-    data_t                                wdata;      // data in case of a write
-    be_t                                  be;         // byte-enable for the write data
+    logic [WORD_SIZE_BITS-1:0]            wdata;      // data in case of a write
+    logic [WORD_SIZE_BITS/BYTE_SIZE_BITS-1:0] be;    // byte-enable for the write data
     logic                                 dirty;      // (Write only) 1 if sector written is dirty, 0 if clean
   } cache_req_t;
 
