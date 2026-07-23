76d197e4e0308cfde8bc65c4956efdda525aa5de
Alain Girard <alain.girard@epfl.ch>
2026-07-23 15:08:46 +0200

    init camera_if

---
+++ sw/device/lib/drivers/camera/camera_regs.h
@@ -0,0 +1,24 @@
+// Generated register defines for camera
+
+#ifndef _CAMERA_REG_DEFS_
+#define _CAMERA_REG_DEFS_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+// Register width
+#define CAMERA_PARAM_REG_WIDTH 32
+
+// Control register for flash controller
+#define CAMERA_CONTROL_REG_OFFSET 0x0
+#define CAMERA_CONTROL_START_BIT 0
+
+// Status register for flash controller
+#define CAMERA_STATUS_REG_OFFSET 0x4
+#define CAMERA_STATUS_READY_BIT 0
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif
+#endif  // _CAMERA_REG_DEFS_
+// End generated register defines for camera
