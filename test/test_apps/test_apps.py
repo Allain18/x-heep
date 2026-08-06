2be6d248e16c7d9f845c42a08b1c28ae06c75b1d
davide schiavone <davide@openhwgroup.org>
2026-08-06 16:01:28 +0200

    update vendor

--- test/test_apps/test_apps.py
+++ test/test_apps/test_apps.py
@@ -54,7 +54,6 @@ BLACKLIST = [
     "example_pdm2pcm",
     "example_dma_slow_mem",  # TODO: @tommaso remove this once it's fixed
     "example_matmul_quadrilatero",
-    "example_w25q128jw_write",
     "example_serial_link_performance",
     "example_serial_link_simulation_dma",  # Functionality already covered by example_serial_link_direct_write, excluded to avoid redundant testing
     "example_serial_link",  # Functionality already covered by example_serial_link_direct_write, excluded to avoid redundant testing
@@ -64,7 +63,6 @@ BLACKLIST = [
     "example_sdk_spi_flash",  # Times out
     "example_freertos_blinky",  # Times out
     "example_matmul",  # Times out
-    "example_w25q128jw_read",  # Times out
 ]
 # TODO : The example_pdm2pcm app is testing a wrong version of the PDM2PCM acting only as a CIC filter.
 #        When fixed, it not passes anymore. Need to be updated.
